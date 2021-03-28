6bf28a2b62ce1935356c3bd9b2e32b91d636a609
Alex Basin <logictester@me.com>
2021-03-27 21:11:03 -0400

    remove rst2pdf

--- source/conf.py
+++ source/conf.py
@@ -39,8 +39,7 @@ extensions = [
     'sphinx_copybutton',
     'sphinx_rtd_theme',
     'sphinx_tabs.tabs',
-    'sphinxcontrib.blockdiag',
-    'rst2pdf.pdfbuilder'
+    'sphinxcontrib.blockdiag'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
