3aa8703328d43cbde4e2f4f7e59ab233f00c7c98
Alex Basin <logictester@me.com>
2021-03-29 10:25:26 -0400

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'SafeNet Trusted Access - Protecting RDGW'
+project = 'SafeNet Trusted Access'
 copyright = '2021, Alex Basin'
 author = 'Alex Basin'
 
